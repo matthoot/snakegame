5a82623fe3d04e6e38b5a801cf64cf6d09b53fe5
matthoot <matt221136@gmail.com>
2023-03-15 22:28:23 -0400

    made objects work

--- snake.py
+++ snake.py
@@ -1,3 +1,4 @@
+
 import pygame
 import copy
 import random
@@ -6,6 +7,7 @@ pygame.init()
 # 1 = light green, 2 = dark green, 3 = wall, 4 = light green with apple, 5 = dark green with apple
 #18 x 16 board, 850x750
 
+direction_command = "none"
 board = [
     [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
     [3,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,3],
@@ -40,52 +42,79 @@ class Orange:
         self.rect.x = 58 + (50 * self.col_index)
         self.rect.y = 158 + (50 * self.row_index)
 
-WIDTH = 950
-HEIGHT = 950
-score_padding = HEIGHT - WIDTH
-snakeHead_x = 255
-snakeHead_y = 505
-directions = { "left": 0, "right": 1, "up": 2, "down": 3, "none": 4}
-direction = directions["none"]
-direction_command = directions["none"]
-last_direction = directions["none"]
-snakeHead_image = pygame.image.load('assets/snake.png')
-orange_image = pygame.image.load('assets/orange.png')
-snake_speed = 5
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-timer = pygame.time.Clock()
-font = pygame.font.Font('freesansbold.ttf',20)
-fps = 60
-level = copy.deepcopy(board)
-rows = 15
-cols = 17
-center_snake_x = snakeHead_x + 20
-center_snake_y = snakeHead_y + 20
-screen.blit(snakeHead_image, (snakeHead_x, snakeHead_y))
-snake_collision_box = pygame.draw.circle(screen, 'red', (center_snake_x, center_snake_y), 20, 2)
-orange = Orange()
-score = 0
-
-orange_collision = False
-
 class Segment:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.next_segment = None
+        self.image = pygame.image.load('assets/snake2.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         self.center_x = x + 20
         self.center_y = y + 20
-        self.next_segment = None
+        self.canGoLeft = False
+        self.canGoRight = False
+        self.canGoUp = False
+        self.canGoDown = False
+        self.direction = "none"
+        self.last_direction = "none"
         
-    def move(self, dx, dy):
-        self.x += dx
-        self.x += dy
+    def move(self):
+        if self.direction == "left" and self.canGoLeft:
+            self.rect.x -= snake_speed
+            self.center_x -= snake_speed
+        elif self.direction == "right" and self.canGoRight:
+            self.rect.x += snake_speed
+            self.center_x += snake_speed
+        elif self.direction == "up" and self.canGoUp:
+            self.rect.y -= snake_speed
+            self.center_y -= snake_speed
+        elif self.direction == "down" and self.canGoDown:
+            self.rect.y += snake_speed
+            self.center_y += snake_speed
+        else:
+            self.direction = self.last_direction
+    
+    def check_direction(self):
+        centerAdjust = 30
+        currentSquare_x = self.center_x - ((self.center_x // 50) * 50)
+        currentSquare_y = self.center_y - ((self.center_y // 50) * 50)
+        if self.direction == "none" and direction_command == "none":
+            self.canGoLeft, self.canGoRight, self.canGoUp, self.canGoDown = True, True, True, True
         
+        if 20 <= currentSquare_x <= 30:
+            self.canGoUp = True
+            self.canGoDown = True
+        else:
+            self.canGoUp = False
+            self.canGoDown = False
+                
+        if 20 <= currentSquare_y <= 30:
+            self.canGoRight = True
+            self.canGoLeft = True
+        else:
+            self.canGoRight = False
+            self.canGoLeft = False
+        
+        if (self.center_x - centerAdjust) < 50:
+            self.canGoLeft = False
+        if (self.center_x + centerAdjust) > 900:
+            self.canGoRight = False
+        if (self.center_y + centerAdjust) < 210:
+            self.canGoUp = False
+        if (self.center_y - centerAdjust) > 840:
+            self.canGoDown = False
+            
 class Snake:
     def __init__(self, x, y):
         self.head = Segment(x, y)
         self.length = 1
         self.tail = self.head
-        self.direction = None
+        # self.direction = "none"
+        # self.last_direction = "none"
+        # self.canGoLeft = False
+        # self.canGoRight = False
+        # self.canGoUp = False
+        # self.canGoDown = False
     
     def moveSegments(self, dx, dy):
         current_segment = self.tail
@@ -98,7 +127,7 @@ class Snake:
     def draw(self, screen):
         segment = self.head
         while segment is not None:
-            screen.blit(segment.surface, segment.rect)
+            screen.blit(segment.image, segment.rect)
             segment = segment.next_segment
             
     def set_direction(self, direction):
@@ -107,6 +136,28 @@ class Snake:
     def move(self):
         if self.direction is None:
             return
+
+        
+
+WIDTH = 950
+HEIGHT = 950
+score_padding = HEIGHT - WIDTH
+snakeHead_x = 255
+snakeHead_y = 505
+snake_speed = 5
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+timer = pygame.time.Clock()
+font = pygame.font.Font('freesansbold.ttf',20)
+fps = 60
+level = copy.deepcopy(board)
+rows = 15
+cols = 17
+center_snake_x = snakeHead_x + 20
+center_snake_y = snakeHead_y + 20
+orange = Orange()
+snake = Snake(255, 505)
+score = 0
+orange_collision = False
 
 def drawboard():
     pygame.draw.rect(screen, (72, 118, 47), pygame.Rect(0, 0, 950, 100)) # top bar with score and stuff
@@ -120,51 +171,40 @@ def drawboard():
             elif board[column][row] == 3:
                 pygame.draw.rect(screen, (84, 140, 56), pygame.Rect(50 * row, 100 + (50 * column), 50, 50))
 
-def move_snake(direction, snakeHead_x, snakeHead_y):
-    if not valid_turns[direction]:
-        direction = last_direction
-    if direction == directions["left"] and valid_turns[directions["left"]]:
-        snakeHead_x -= snake_speed
-    elif direction == directions["right"] and valid_turns[directions["right"]]:
-        snakeHead_x += snake_speed
-    elif direction == directions["up"] and valid_turns[directions["up"]]:
-        snakeHead_y -= snake_speed
-    elif direction == directions["down"] and valid_turns[directions["down"]]:
-        snakeHead_y += snake_speed
-    return direction, snakeHead_x, snakeHead_y    
-
 def check_position(center_x, center_y):
-    valid_turns = [False, False, False, False, False]  
+    left, right, up, down, none = False, False, False, False, False 
     centerAdjust = 30
     currentSquare_x = center_x - ((center_x // 50) * 50)
     currentSquare_y = center_y - ((center_y // 50) * 50)
-    if direction == directions["none"] and direction_command == directions["none"]:
-        return [True, True, True, True, False]
+    if direction == "none" and direction_command == "none":
+        left, right, up, down = True, True, True, True
+        none = False
+        return left, right, up, down, none
     
     if 20 <= currentSquare_x <= 30:
-        valid_turns[directions["up"]] = True
-        valid_turns[directions["down"]] = True
+        up = True
+        down = True
     else:
-        valid_turns[directions["up"]] = False
-        valid_turns[directions["down"]] = False
+        up = False
+        down = False
             
     if 20 <= currentSquare_y <= 30:
-        valid_turns[directions["right"]] = True
-        valid_turns[directions["left"]] = True
+        right = True
+        left = True
     else:
-        valid_turns[directions["right"]] = False
-        valid_turns[directions["left"]] = False
+        right = False
+        left = False
     
     if (center_x - centerAdjust) < 50:
-        valid_turns[directions["left"]] = False
+        left = False
     if (center_x + centerAdjust) > 900:
-        valid_turns[directions["right"]] = False
+        right = False
     if (center_y + centerAdjust) < 210:
-        valid_turns[directions["up"]] = False
+        up = False
     if (center_y - centerAdjust) > 840:
-        valid_turns[directions["down"]] = False
+        down = False
         
-    return valid_turns   
+    return left, right, up, down, none
 
 def draw_misc():
     score_text = font.render(f'Score: {score}', True, 'white')
@@ -178,37 +218,34 @@ while run:
     drawboard()
     draw_misc()
     orange.draw(screen)
-    if snake_collision_box.colliderect(orange.rect):
+    snake.draw(screen)
+    if snake.head.rect.colliderect(orange.rect):
         orange.randomize()
         score += 1
-    center_snake_x = snakeHead_x + 20
-    center_snake_y = snakeHead_y + 20
-    snake_collision_box = pygame.draw.circle(screen, 'black', (center_snake_x, center_snake_y), 20, 2)
-    screen.blit(snakeHead_image, (snakeHead_x, snakeHead_y))
-    valid_turns = check_position(center_snake_x, center_snake_y)
-    direction, snakeHead_x, snakeHead_y = move_snake(direction, snakeHead_x, snakeHead_y)
+    snake.head.check_direction()
+    snake.head.move()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                direction_command = directions["right"]
+                direction_command = "right"
             if event.key == pygame.K_LEFT:
-                direction_command = directions["left"]
+                direction_command = "left"
             if event.key == pygame.K_UP:
-                direction_command = directions["up"]
+                direction_command = "up"
             if event.key == pygame.K_DOWN:
-                direction_command = directions["down"]
+                direction_command = "down"
     
-    last_direction = direction
-    if direction_command == directions["right"] and valid_turns[directions["right"]]:
-        direction = directions["right"]
-    if direction_command == directions["left"] and valid_turns[directions["left"]]:
-        direction = directions["left"]
-    if direction_command == directions["up"] and valid_turns[directions["up"]]:
-        direction = directions["up"]
-    if direction_command == directions["down"] and valid_turns[directions["down"]]:
-        direction = directions["down"]  
+    snake.head.last_direction = snake.head.direction
+    if direction_command == "right" and snake.head.canGoRight:
+        snake.head.direction = "right"
+    if direction_command == "left" and snake.head.canGoLeft:
+        snake.head.direction = "left"
+    if direction_command == "up" and snake.head.canGoUp:
+        snake.head.direction = "up"
+    if direction_command == "down" and snake.head.canGoDown:
+        snake.head.direction = "down"
         
     pygame.display.flip()
     
